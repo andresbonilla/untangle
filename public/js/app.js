4bd51e137cb6df5a998458fdf199f416c2c396f5
Andres Bonilla <andresbonilla@gmail.com>
2012-06-13 12:53:47 -0400

    Added win condition check and puzzle growth function in server. The game is functionally complete.

--- public/js/app.js
+++ public/js/app.js
@@ -1,85 +1,100 @@
 /*
-	App
+  App
 */
 Untangle = Ember.Application.create({
-	POINT_RADIUS: 10,
-  HELD_POINT_RADIUS: 15,
-	POINT_FILL_COLOR: '#0F0',
-	HELD_POINT_FILL_COLOR: '#F00',
-	POINT_STROKE_COLOR: '#000',
-	HELD_POINT_STROKE_COLOR: '#000',
-	LINE_COLOR: '#000',
-	LINE_STROKE_WIDTH: 4,
-	POINT_STROKE_WIDTH: 2,
-	XMIN: 20,
-	XMAX: 500,
-	YMIN: 20,
-	YMAX: 500,
-	
-	socket: io.connect(location.protocol + '//' + location.hostname),
-	holding: null,
-	
-	ready: function () {
-		Untangle.socket.on('init', function (data) {
-      Untangle.pointsController.loadPoints(data.points);
-      Untangle.linesController.loadLines(data.lines);
-      Untangle.linesController.drawAll();
-      Untangle.pointsController.drawAll();
-    });
+    POINT_RADIUS: 10,
+    HELD_POINT_RADIUS: 15,
+    POINT_FILL_COLOR: '#0F0',
+    HELD_POINT_FILL_COLOR: '#F00',
+    POINT_STROKE_COLOR: '#000',
+    HELD_POINT_STROKE_COLOR: '#000',
+    LINE_COLOR: '#000',
+    LINE_STROKE_WIDTH: 4,
+    POINT_STROKE_WIDTH: 2,
+    XMIN: 20,
+    XMAX: 500,
+    YMIN: 20,
+    YMAX: 500,
 
-		Untangle.socket.on('held', function (data) {
-			Untangle.pointsController.pointHeld(data.point_id);
-		});
-		
-		Untangle.socket.on('moved', function (data) {
-			Untangle.pointsController.pointMoved(data.point_id, data.x, data.y);
-		});
-	
-		Untangle.socket.on('released', function (data) {
-			Untangle.pointsController.pointReleased(data.point_id);
-		});
-	}
+    socket: io.connect(location.protocol + '//' + location.hostname),
+    holding: null,
+
+    ready: function () {
+        Untangle.socket.on('init', function (data) {
+            Untangle.pointsController.loadPoints(data.points);
+            Untangle.linesController.loadLines(data.lines);
+            Untangle.linesController.drawAll();
+            Untangle.pointsController.drawAll();
+        });
+
+        Untangle.socket.on('held', function (data) {
+            Untangle.pointsController.pointHeld(data.point_id);
+        });
+
+        Untangle.socket.on('moved', function (data) {
+            Untangle.pointsController.pointMoved(data.point_id, data.x, data.y);
+        });
+
+        Untangle.socket.on('released', function (data) {
+            Untangle.pointsController.pointReleased(data.point_id);
+        });
+        
+        Untangle.socket.on('solved', function (data) {
+            Untangle.pointsController.loadPoints(data.newPoints);
+            Untangle.linesController.loadLines(data.newLines);
+            Untangle.linesController.drawAll();
+            Untangle.pointsController.drawAll();
+        });
+    }
 });
 
 
 /*
-	Model
+  Model
 */
 Untangle.Point = Ember.Object.extend({
     id: null,
     x: null,
     y: null,
-		held: false,
-		
+    held: false,
+
     hold: function () {
-			Untangle.holding = this.id;
-			this.wasHeld();
-			Untangle.socket.emit('hold', { point_id: this.id });
-		},
+        Untangle.holding = this.id;
+        this.wasHeld();
+        Untangle.socket.emit('hold', {
+            point_id: this.id
+        });
+    },
 
-		wasHeld: function () {
-			this.held = true;
-		},
-		
-		move: function (x, y) {
-			this.wasMoved(x, y);
-			Untangle.socket.emit('move', { point_id: this.id, x: this.x, y: this.y });
-		},
+    wasHeld: function () {
+        this.held = true;
+    },
 
-		wasMoved: function (x, y) {
-			this.x = x;
-			this.y = y;
-		},
-		
+    move: function (x, y) {
+        this.wasMoved(x, y);
+        Untangle.socket.emit('move', {
+            point_id: this.id,
+            x: this.x,
+            y: this.y
+        });
+    },
+
+    wasMoved: function (x, y) {
+        this.x = x;
+        this.y = y;
+    },
+
     release: function () {
-			Untangle.holding = null;
-			this.wasReleased();
-			Untangle.socket.emit('release', {point_id: this.id});
-		},
-		
-		wasReleased: function () {
-			this.held = false;
-		}	
+        Untangle.holding = null;
+        this.wasReleased();
+        Untangle.socket.emit('release', {
+            point_id: this.id
+        });
+    },
+
+    wasReleased: function () {
+        this.held = false;
+    }
 });
 
 Untangle.Line = Ember.Object.extend({
@@ -89,10 +104,10 @@ Untangle.Line = Ember.Object.extend({
 
 
 /*
-	Controllers
+  Controllers
 */
 Untangle.pointsController = Ember.ArrayProxy.create({
-		content: [],
+    content: [],
 
     create: function (id, x, y) {
         var point = Untangle.Point.create({
@@ -101,11 +116,11 @@ Untangle.pointsController = Ember.ArrayProxy.create({
             y: y
         });
         this.pushObject(point);
-				return point;
+        return point;
     },
 
     loadPoints: function (points) {
-				this.set('content', []);
+        this.set('content', []);
         var point;
         for (var i = 0; i < points.length; i++) {
             point = points[i];
@@ -114,80 +129,84 @@ Untangle.pointsController = Ember.ArrayProxy.create({
     },
 
     drawAll: function () {
-			d3.select('svg')
-			  .selectAll('circle')
-			  .data(this.get('content'))
-			  .enter()
-			  .append('circle')
-			  .attr('id', function (d) { return d.id; })
-			  .attr('cx', function (d) { return d.x; })
-			  .attr('cy', function (d) { return d.y; })
-			  .attr('r', function (d) { return (d.held ? Untangle.HELD_POINT_RADIUS : Untangle.POINT_RADIUS); })
-			  .style('fill', function (d) { return (d.held ? Untangle.HELD_POINT_FILL_COLOR : Untangle.POINT_FILL_COLOR); })
-			  .style('stroke', function (d) { return (d.held ? Untangle.HELD_POINT_STROKE_COLOR : Untangle.POINT_STROKE_COLOR); })
-			  .style('stroke-width', Untangle.POINT_STROKE_WIDTH)
-			  .call(d3.behavior.drag()
-		        .on("dragstart", function (d, i) {
-								if (!d.held) {
-		            	d.hold();
-									d3.select(this).attr('r', Untangle.HELD_POINT_RADIUS).style("fill", Untangle.HELD_POINT_FILL_COLOR);
-								}
-		        })
-		        .on("drag", function (d, i) {
-								if (d.held && Untangle.holding === d.id) {
-			            d.move(d.x + d3.event.dx, d.y + d3.event.dy);
-			            d3.select(this).attr('cx', d.x);
-			            d3.select(this).attr('cy', d.y);
-			            Untangle.linesController.update();
-								}
-		        })
-		        .on("dragend", function (d, i) {
-								if (d.held && Untangle.holding === d.id) {
-		            	d.release();
-		            	d3.select(this).attr('r', Untangle.POINT_RADIUS).style("fill", Untangle.POINT_FILL_COLOR);
-								}
-		        })
-				);
+        d3.selectAll('circle').remove();
+      
+        d3.select('svg')
+          .selectAll('circle')
+          .data(this.get('content'))
+          .enter()
+          .append('circle')
+          .attr('id', function (d) { return d.id; })
+          .attr('cx', function (d) { return d.x; })
+          .attr('cy', function (d) { return d.y; })
+          .attr('r', function (d) { return (d.held ? Untangle.HELD_POINT_RADIUS : Untangle.POINT_RADIUS); })
+          .style('fill', function (d) { return (d.held ? Untangle.HELD_POINT_FILL_COLOR : Untangle.POINT_FILL_COLOR); })
+          .style('stroke', function (d) { return (d.held ? Untangle.HELD_POINT_STROKE_COLOR : Untangle.POINT_STROKE_COLOR); })
+          .style('stroke-width', Untangle.POINT_STROKE_WIDTH)
+          .call(d3.behavior.drag()
+            .on("dragstart", function (d, i) {
+              if (!d.held) {
+                d.hold();
+                d3.select(this)
+                  .attr('r', Untangle.HELD_POINT_RADIUS)
+                  .style("fill", Untangle.HELD_POINT_FILL_COLOR);
+              }
+            })
+            .on("drag", function (d, i) {
+              if (d.held && Untangle.holding === d.id) {
+                d.move(d.x + d3.event.dx, d.y + d3.event.dy);
+                d3.select(this).attr('cx', d.x);
+                d3.select(this).attr('cy', d.y);
+                Untangle.linesController.update();
+              }
+            })
+            .on("dragend", function (d, i) {
+              if (d.held && Untangle.holding === d.id) {
+                d.release();
+                d3.select(this)
+                  .attr('r', Untangle.POINT_RADIUS)
+                  .style("fill", Untangle.POINT_FILL_COLOR);
+              }
+            }));
     },
-		
-		find: function (point_id) {
-			var points = this.get('content');
-			for (var i = 0; i < points.length; i++) {
-				if (points[i].id === point_id) {
-					return points[i];
-				}
-			}
-		},
-		
-		pointHeld: function (point_id) {
-			var point = Untangle.pointsController.find(point_id);
-			point.wasHeld();
-			d3.select('circle[id="'+point.id+'"]')
-			  .attr('r', Untangle.HELD_POINT_RADIUS)
-			  .style('fill', Untangle.HELD_POINT_FILL_COLOR)
-			  .style('stroke', Untangle.HELD_POINT_STROKE_COLOR);
-		},
-		
-		pointMoved: function (point_id, x, y) {
-			var point = Untangle.pointsController.find(point_id);
-			point.wasMoved(x, y);
-			d3.select('circle[id="'+point.id+'"]')
-		  .attr('cx', x)
-		  .attr('cy', y)
-		  .attr('r', Untangle.HELD_POINT_RADIUS)
-		  .style('fill', Untangle.HELD_POINT_FILL_COLOR)
-		  .style('stroke', Untangle.HELD_POINT_STROKE_COLOR)
-		  Untangle.linesController.update();
-		},
-		
-		pointReleased: function (point_id) {
-			var point = Untangle.pointsController.find(point_id);
-			point.wasReleased();
-			d3.select('circle[id="'+point.id+'"]')
-		  	.attr('r', Untangle.POINT_RADIUS)
-		  	.style('fill', Untangle.POINT_FILL_COLOR)
-		  	.style('stroke', Untangle.POINT_STROKE_COLOR);
-		}
+
+    find: function (point_id) {
+        var points = this.get('content');
+        for (var i = 0; i < points.length; i++) {
+            if (points[i].id === point_id) {
+                return points[i];
+            }
+        }
+    },
+
+    pointHeld: function (point_id) {
+        var point = Untangle.pointsController.find(point_id);
+        point.wasHeld();
+        d3.select('circle[id="' + point.id + '"]')
+          .attr('r', Untangle.HELD_POINT_RADIUS)
+          .style('fill', Untangle.HELD_POINT_FILL_COLOR)
+          .style('stroke', Untangle.HELD_POINT_STROKE_COLOR);
+    },
+
+    pointMoved: function (point_id, x, y) {
+        var point = Untangle.pointsController.find(point_id);
+        point.wasMoved(x, y);
+        d3.select('circle[id="' + point.id + '"]')
+          .attr('cx', x).attr('cy', y)
+          .attr('r', Untangle.HELD_POINT_RADIUS)
+          .style('fill', Untangle.HELD_POINT_FILL_COLOR)
+          .style('stroke', Untangle.HELD_POINT_STROKE_COLOR);
+        Untangle.linesController.update();
+    },
+
+    pointReleased: function (point_id) {
+        var point = Untangle.pointsController.find(point_id);
+        point.wasReleased();
+        d3.select('circle[id="' + point.id + '"]')
+          .attr('r', Untangle.POINT_RADIUS)
+          .style('fill', Untangle.POINT_FILL_COLOR)
+          .style('stroke', Untangle.POINT_STROKE_COLOR);
+    }
 });
 
 Untangle.linesController = Ember.ArrayProxy.create({
@@ -199,21 +218,22 @@ Untangle.linesController = Ember.ArrayProxy.create({
             point2: point2
         });
         this.pushObject(line);
-				return line;
+        return line;
     },
-		
+
     loadLines: function (lines) {
-				this.set('content', []);
-        var line, point1, point2, i;
-        for (i = 0; i < lines.length; i++) {
+        this.set('content', []);
+        var line, point1, point2;
+        for (var i = 0; i < lines.length; i++) {
             line = lines[i];
-            point1 = Untangle.pointsController.objectAt(line.point1);
-            point2 = Untangle.pointsController.objectAt(line.point2);
+            point1 = Untangle.pointsController.find(line.point1.id);
+            point2 = Untangle.pointsController.find(line.point2.id);
             this.create(point1, point2);
         }
     },
 
     drawAll: function () {
+        d3.selectAll('line').remove();
         d3.select('svg')
           .selectAll('line')
           .data(Untangle.linesController.get('content'))
@@ -221,7 +241,7 @@ Untangle.linesController = Ember.ArrayProxy.create({
           .append('line')
           .style('stroke', Untangle.LINE_COLOR)
           .style('stroke-width', Untangle.LINE_STROKE_WIDTH)
-					.attr('x1', function (d) { return d.point1.x; })
+          .attr('x1', function (d) { return d.point1.x; })
           .attr('y1', function (d) { return d.point1.y; })
           .attr('x2', function (d) { return d.point2.x; })
           .attr('y2', function (d) { return d.point2.y; });
@@ -229,7 +249,7 @@ Untangle.linesController = Ember.ArrayProxy.create({
 
     update: function () {
         d3.selectAll('line')
-        	.data(Untangle.linesController.get('content'))
+          .data(Untangle.linesController.get('content'))
           .attr('x1', function (d) { return d.point1.x; })
           .attr('y1', function (d) { return d.point1.y; })
           .attr('x2', function (d) { return d.point2.x; })
--- server.js
+++ server.js
@@ -1,108 +1,136 @@
-/*
+(function () {
+  /*
 
-	Fixtures
+  	Fixtures
 
-*/
-var samplePoints = [
-    { id: 0, x: 320, y: 230, held: false },
-    { id: 1, x: 124, y: 251, held: false },
-    { id: 2, x: 145, y: 454, held: false },
-    { id: 3, x: 70, y: 330, held: false }
-];
+  */
+  var samplePoints = [
+      { id: 0, x: 320, y: 230, held: false },
+      { id: 1, x: 124, y: 251, held: false },
+      { id: 2, x: 145, y: 454, held: false },
+      { id: 3, x: 70, y: 330, held: false }
+  ];
 
-var sampleLines = [
-    { point1: 0, point2: 1 },
-    { point1: 1, point2: 2 },
-    { point1: 2, point2: 3 },
-    { point1: 3, point2: 0 },
-    { point1: 0, point2: 2 }
-];
-
-var holds = {}
-
-var express = require('express'),
-    app     = express.createServer(),
-    io      = require('socket.io').listen(app);
-
-app.get("/", function(req, res) {
-    res.redirect("/index.html");
-});
-
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({
-    dumpExceptions: true, 
-    showStack: true
-  }));
-  app.use(app.router);
-});
-
-io.configure(function () { 
-  io.set('transports', ['xhr-polling']); 
-  io.set('polling duration', 10); 
-});
-
-io.sockets.on('connection', function (socket) {
+  var sampleLines = [
+      { point1: samplePoints[0], point2: samplePoints[1] },
+      { point1: samplePoints[1], point2: samplePoints[2] },
+      { point1: samplePoints[2], point2: samplePoints[3] },
+      { point1: samplePoints[3], point2: samplePoints[0] },
+      { point1: samplePoints[0], point2: samplePoints[2] }
+  ];
   
-  socket.emit('init', { points: samplePoints, lines: sampleLines });
-  
-  socket.on('hold', function (data) {
-	  holds[socket.id] = data.point_id;
-    var curr;
-    for (var i = 0; i < samplePoints.length; i++) {
-      curr = samplePoints[i];
-      if (curr.id === data.point_id) {
-        curr.held = true;
-        socket.broadcast.emit('held', { point_id: curr.id });
-        break;
-      }
-    }
+  var XMIN = 20,
+      XMAX = 500,
+      YMIN = 20,
+      YMAX = 500;
+
+  var express  = require('express'),
+      app      = express.createServer(),
+      io       = require('socket.io').listen(app),
+      geometry = require('./lib/geometry.js'),
+      holds    = {};
+
+  app.get("/", function(req, res) {
+      res.redirect("/index.html");
   });
 
-  socket.on('move', function (data) {
-    var curr;
-    for (var i = 0; i < samplePoints.length; i++) {
-      curr = samplePoints[i];
-      if (curr.id === data.point_id) {
-        curr.x = data.x;
-				curr.y = data.y;
-        socket.broadcast.emit('moved', { point_id: curr.id, x: curr.x, y: curr.y });
-        break;
-      }
-    }
-  });
-
-  socket.on('release', function (data) {
-    var curr;
-    for (var i = 0; i < samplePoints.length; i++) {
-      curr = samplePoints[i];
-      if (curr.id === data.point_id) {
-        curr.held = false;
-        socket.broadcast.emit('released', { point_id: curr.id });
-        break;
-      }
-    }
-    // TODO: Check for win condition and broadcast victory if found or write point positions to DB
-	  delete holds[socket.id];
+  app.configure(function (){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({
+      dumpExceptions: true, 
+      showStack: true
+    }));
+    app.use(app.router);
   });
   
-	socket.on('disconnect', function () {
+  function shufflePoints () {
+    for (var i = 0; i < samplePoints.length; i++) {
+      samplePoints[i].x = Math.floor((Math.random()*XMAX)) + XMIN;
+      samplePoints[i].y = Math.floor((Math.random()*YMAX)) + YMIN;
+    }
+  }
+  
+  function grow () {
+    var randomLine = sampleLines[Math.floor((Math.random()*sampleLines.length))],
+        newPoint = {id: samplePoints.length, x: 0, y:0, held:false};
+    samplePoints.push(newPoint);
+    sampleLines.push({point1: newPoint, point2: randomLine.point1});
+    sampleLines.push({point1: newPoint, point2: randomLine.point2});
+    shufflePoints();
+  }
+  
+  io.configure(function () { 
+    io.set('transports', ['xhr-polling']); 
+    io.set('polling duration', 10); 
+  });
 
-	  var curr,
-				held_point = holds[socket.id];
-	  for (var i = 0; i < samplePoints.length; i++) {
-	    curr = samplePoints[i];
-	    if (curr.id === held_point) {
-	      curr.held = false;
-	      socket.broadcast.emit('released', { point_id: curr.id });
-	      break;
-	    }
-	  }
-	  // TODO: Check for win condition and broadcast victory if found or write point positions to DB
-		delete holds[socket.id];
-	});
-});
+  io.sockets.on('connection', function (socket) {
+  
+    socket.emit('init', { points: samplePoints, lines: sampleLines });
+  
+    socket.on('hold', function (data) {
+      holds[socket.id] = data.point_id;
+      var curr;
+      for (var i = 0; i < samplePoints.length; i++) {
+        curr = samplePoints[i];
+        if (curr.id === data.point_id) {
+          curr.held = true;
+          socket.broadcast.emit('held', { point_id: curr.id });
+          break;
+        }
+      }
+    });
 
-app.listen(process.env.PORT || 4000);
+    socket.on('move', function (data) {
+      var curr;
+      for (var i = 0; i < samplePoints.length; i++) {
+        curr = samplePoints[i];
+        if (curr.id === data.point_id) {
+          curr.x = data.x;
+          curr.y = data.y;
+          socket.broadcast.emit('moved', { point_id: curr.id, x: curr.x, y: curr.y });
+          break;
+        }
+      }
+    });
+
+    socket.on('release', function (data) {
+      var curr;
+      for (var i = 0; i < samplePoints.length; i++) {
+        curr = samplePoints[i];
+        if (curr.id === data.point_id) {
+          curr.held = false;
+          socket.broadcast.emit('released', { point_id: curr.id });
+          break;
+        }
+      }
+      delete holds[socket.id];
+      if (!geometry.intersectionsFound(sampleLines)) {
+          grow();
+          io.sockets.emit('solved', { newPoints: samplePoints, newLines: sampleLines });
+      }
+    });
+  
+    socket.on('disconnect', function () {
+      var curr,
+          held_point = holds[socket.id];
+      for (var i = 0; i < samplePoints.length; i++) {
+        curr = samplePoints[i];
+        if (curr.id === held_point) {
+          curr.held = false;
+          socket.broadcast.emit('released', { point_id: curr.id });
+          break;
+        }
+      }
+      delete holds[socket.id];
+      if (!geometry.intersectionsFound(sampleLines)) {
+          grow();
+          io.sockets.emit('solved', { newPoints: samplePoints, newLines: sampleLines });
+      }
+    });
+  });
+
+  app.listen(process.env.PORT || 4000);
+}());
